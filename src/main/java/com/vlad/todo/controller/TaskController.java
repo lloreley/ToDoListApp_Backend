e03f3d42353fafa0c709527a00b3ea7b55131825
lloreley <supervoin.d1@gmail.com>
2025-02-20 20:40:31 +0300

    5th commit

--- src/main/java/com/vlad/todo/controller/TaskController.java
+++ src/main/java/com/vlad/todo/controller/TaskController.java
@@ -1,20 +1,21 @@
 package com.vlad.todo.controller;
 
 
+import com.vlad.todo.model.Task;
+//import java.time.LocalDate;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.vlad.todo.model.Task;
 
-import java.time.LocalDate;
-import java.util.List;
+
 
 @RestController
 @RequestMapping  ("/api/v1/tasks")
 public class TaskController {
-
-    public List<Task> findAllTasks(){
+    public List<Task> findAllTasks() {
         return List.of(
-          new Task("Eat", LocalDate.now())
+                new Task("Sleep")
         );
     }
+
 }
--- src/main/java/com/vlad/todo/model/Task.java
+++ src/main/java/com/vlad/todo/model/Task.java
@@ -3,13 +3,18 @@ package com.vlad.todo.model;
 import java.time.LocalDate;
 
 public class Task {
-    private String Content;
-    private LocalDate DateOfCreation;
-    private LocalDate DateOfCompletion;
+    private String content;
 
-    public Task(String content, LocalDate dateOfCreation) {
-        this.Content = content;
-        this.DateOfCreation = dateOfCreation;
+    public Task(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 
 }
